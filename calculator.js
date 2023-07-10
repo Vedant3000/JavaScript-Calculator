7ae6ba1216ee4f3fa9b77eadfe3e9ad862dec9bf
Vedant3000 <87371920+Vedant3000@users.noreply.github.com>
2023-07-10 20:29:57 +0530

    Add files via upload

---
+++ calculator.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const app = express();
+
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.get("/", function(req, res){
+    res.sendFile(__dirname + "/index.html");
+});
+
+app.post('/', function(req, res){
+
+var num1 = Number(req.body.num1);
+var num2 = Number(req.body.num2);
+var res = num1+num2;
+    res.send("Result: " + res);
+});
+
+app.get("/bmicalculator", function(req, res){
+    res.sendFile(__dirname + "/bmiCalc.html");
+});
+
+app.post("/bmicalculator", function(req, res){
+    var w = Number(req.body.weight);
+    var h = Number(req.body.height);
+    var r = w/(h*h);
+    res.send("Your BMI is: " +r);
+})
+
+app.listen(3000, function(){
+    console.log("Server started");
+});
